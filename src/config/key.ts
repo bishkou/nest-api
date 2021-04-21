8b04f9f27445b046451579e29b75916d437a0411
bishkou <chedyhammami@yahoo.com>
2021-04-21 13:33:24 +0100

    crud

---
+++ src/config/key.ts
@@ -0,0 +1,3 @@
+export default {
+  mongoURI: "mongodb+srv://webipie:webipie@webipie.ziihb.mongodb.net/webipie?retryWrites=true&w=majority"
+}
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,14 +1,36 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item-dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {
+  }
+
   @Get()
-  getAll(): string {
-    return 'all items';
+  async findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
   }
 
   @Post()
-  create(): string {
-    return 'create';
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto,
+         @Param('id') id): string {
+    return `update ${id} ${updateItemDto.name}`;
   }
 }
