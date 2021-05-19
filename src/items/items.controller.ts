c2723e86dac78cb449f3b35243f638a330139989
bishkou <chedyhammami@yahoo.com>
2021-05-19 10:31:52 +0100

    api

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -17,7 +17,6 @@ export class ItemsController {
     if (this.yt){
     const ch = await this.yt.channelDetails('UC8butISFwT-Wl7EV0hUK0BQ');
       // console.log(ch);
-    // const vid = await this.yt.videosList(ch.uploads_id, [])
     const c = await this.yt.videoInfo('RBSGKlAvoiM')
       console.log(c);
     return this.itemsService.findAll();
