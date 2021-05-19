a724638b45368e5a5c640bbfa5a489e844b4c75f
bishkou <chedyhammami@yahoo.com>
2021-05-19 10:32:06 +0100

    api

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -16,7 +16,6 @@ export class ItemsController {
   async findAll(): Promise<Item[]> {
     if (this.yt){
     const ch = await this.yt.channelDetails('UC8butISFwT-Wl7EV0hUK0BQ');
-      // console.log(ch);
     const c = await this.yt.videoInfo('RBSGKlAvoiM')
       console.log(c);
     return this.itemsService.findAll();
